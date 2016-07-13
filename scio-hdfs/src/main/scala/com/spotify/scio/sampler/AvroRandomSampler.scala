eea607fea0225e55ec92ece8ebd3e52665994346
Neville Li <neville@spotify.com>
2016-07-13 11:35:03 -0400

    wip

--- scio-hdfs/src/main/scala/com/spotify/scio/sampler/AvroRandomSampler.scala
+++ scio-hdfs/src/main/scala/com/spotify/scio/sampler/AvroRandomSampler.scala
@@ -20,13 +20,14 @@ package com.spotify.scio.sampler
 import org.apache.avro.file.DataFileReader
 import org.apache.avro.generic.{GenericDatumReader, GenericRecord}
 import org.apache.hadoop.conf.Configuration
-import org.apache.hadoop.fs.{AvroFSInput, FileContext, FileSystem, Path}
+import org.apache.hadoop.fs._
 import org.slf4j.{Logger, LoggerFactory}
 
 import scala.collection.mutable.{ListBuffer, Set => MSet}
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, Future}
+import scala.util.Random
 
 class AvroRandomSampler(path: Path, protected val seed: Option[Long] = None)
   extends RandomSampler[GenericRecord] {
@@ -41,15 +42,36 @@ class AvroRandomSampler(path: Path, protected val seed: Option[Long] = None)
     if (fs.isFile(path)) {
       new AvroFileRandomSampler(path, seed).sample(n)
     } else {
-      val statuses = fs.listStatus(path, SamplerUtils.extensionPathFilter(".avro"))
+      val filter = new PathFilter {
+        override def accept(path: Path): Boolean = path.getName.endsWith(".avro")
+      }
+      val statuses = fs.listStatus(path, filter)
       val paths = statuses.map(_.getPath)
       val sizes = statuses.map(_.getLen)
-      val samples = SamplerUtils.scaleWeights(sizes, n)
+      val samples = scaleWeights(sizes, n)
       val futures = paths.zip(samples).map { case (p, s) =>
         Future(new AvroFileRandomSampler(p).sample(s))
       }.toSeq
       Await.result(Future.sequence(futures), Duration.Inf).flatten
     }
+  }
+
+  // Scale weights so that they sum up to n
+  private def scaleWeights(weights: Array[Long], n: Long): Array[Long] = {
+    val sum = weights.sum
+    require(sum > n, "sum of weights must be > n")
+    val result = weights.map(x => (x.toDouble / sum * n).toLong)
+
+    val delta = n - result.sum  // delta due to rounding error
+    var i = 0
+    val dim = weights.length
+    while (i < delta) {
+      // randomly distribute delta
+      result(Random.nextInt(dim)) += 1
+      i+= 1
+    }
+
+    result
   }
 
 }
